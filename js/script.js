f99c528e21bf6ab83878477ec1410bd3829e0fd2
Adil Shabab <adilshaba@gmail.com>
2022-08-04 05:06:46 -0700

    home page completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ var swiper = new Swiper(".mySwiper", {
     autoplay : true,
     delay : 1000
   });
+
+
+// var phone_icon = document.querySelector('.icon2')
+// var phone_text = document.querySelector('.icon-text1')
+
+// var email_icon = document.querySelector('.icon1')
+// var email_text = document.querySelector('.icon-text2')
+
+
+
+// phone_icon.addEventListener('mouseover', function(){
+//     phone_text.classList.toggle('d-block')
+// })
+// email_icon.addEventListener('mouseover', function(){
+//     email_text.classList.toggle('d-block')
+// })
