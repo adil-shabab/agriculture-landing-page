8632d9cd52e7abf81ec02f9146f2a884d20dfb45
Adil Shabab <adilshaba@gmail.com>
2022-08-05 00:14:57 -0700

    progressing

--- js/script.js
+++ js/script.js
@@ -5,6 +5,29 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
+var image1 = document.getElementById('image1')
+var image2 = document.getElementById('image2')
+
+
+var span1 = document.getElementById('span1')
+var span2 = document.getElementById('span2')
+
+image1.addEventListener('mouseover', function(){
+  span1.style.display='block'
+})
+
+image1.addEventListener('mouseleave', function(){
+  span1.style.display='none'
+})
+
+image2.addEventListener('mouseover', function(){
+  span2.style.display='block'
+})
+
+image2.addEventListener('mouseleave', function(){
+  span2.style.display='none'
+})
+
 // var phone_icon = document.querySelector('.icon2')
 // var phone_text = document.querySelector('.icon-text1')
 
